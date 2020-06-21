4ad7f16726f3db023edaf102cfaa60196a4a1ff9
robertyu1636 <robertyu1636@gmail.com>
2020-06-20 21:57:16 -0700

    Updates

---
+++ precache-manifest.6acf34ceabeff60854c79e3ada0791cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2fc775bc1888363d80121313ff173f",
+    "url": "/siyuan-robert-yu/index.html"
+  },
+  {
+    "revision": "4fd2e97ff3a6a26f70a0",
+    "url": "/siyuan-robert-yu/static/css/main.d91e728f.chunk.css"
+  },
+  {
+    "revision": "0e762aa8d0248ba2c05f",
+    "url": "/siyuan-robert-yu/static/js/2.a57e6ac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/siyuan-robert-yu/static/js/2.a57e6ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd2e97ff3a6a26f70a0",
+    "url": "/siyuan-robert-yu/static/js/main.bd1cbe4b.chunk.js"
+  },
+  {
+    "revision": "e21eae376e5e598e5219",
+    "url": "/siyuan-robert-yu/static/js/runtime-main.573fe690.js"
+  },
+  {
+    "revision": "31a7337f5b8bf69c947e37f9da0e41c7",
+    "url": "/siyuan-robert-yu/static/media/Profile.31a7337f.jpg"
+  },
+  {
+    "revision": "c3148119510457ffdd0abb5905953fe9",
+    "url": "/siyuan-robert-yu/static/media/under_construction.c3148119.jpg"
+  }
+]);
